867a4b333fa86b5ab71957ec9ae7b85393ea838e
Matthew Wang <wmatthew123@gmail.com>
2020-09-18 17:29:05 +0800

    added queries for new database

--- routes/cards.js
+++ routes/cards.js
@@ -1,21 +1,38 @@
 const express = require('express');
 const router = express.Router();
 
-const client = require('../lib/mongodb')
+const createClient = require('../lib/mongodb')
 
-/* GET home page. */
-router.get('/', async (req, res, next) => {
+router.get('/:series', async (req, res) => {
+    const client = createClient()
     try {
         await client.connect();
         const collection = client.db('ptcg').collection('cards');
-        const data = await collection.find(req.body).toArray();
-        res.send(data);
+        const cursor = await collection.find(req.params);
+        const pageLength = req.query.pageLength ? parseInt(req.query.pageLength) : 20;
+        const pageIdx = req.query.page ? parseInt(req.query.page) : 0;
+        console.log(req.query);
+        const page = await cursor.skip(pageLength * pageIdx).limit(pageLength).toArray();
+        res.send(page);
     } catch (e) {
-        res.status(404).end(e);
+        res.status(500).end(e.toString());
+    } finally {
+        client.close()
+    }
+});
+
+router.get('/:series/:number', async (req, res, next) => {
+    const client = createClient()
+    try {
+        await client.connect();
+        const collection = client.db('ptcg').collection('cards');
+        const card = await collection.findOne(req.params);
+        res.send(card);
+    } catch (e) {
+        res.status(500).end(e.toString());
     } finally {
         client.close();
     }
-
 });
 
 module.exports = router;
